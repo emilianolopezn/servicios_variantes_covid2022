844f6c794075ae11c95608653dd89b5836e78b95
Ulsa <ulsa@ulsa.com>
2022-04-25 18:50:38 -0700

    api

--- app/Http/Controllers/Api/VariantesController.php
+++ app/Http/Controllers/Api/VariantesController.php
@@ -10,8 +10,19 @@ use App\Variante;
 class VariantesController extends Controller
 {
     public function index() {
-        $variantes = Variante::orderBy('id','DESC')->
+        $variantes = Variante::select('id','lineage',
+        'common_countries','earliest_date',
+        'designated_number','assigned_number',
+        'who_name')->
+            orderBy('id','DESC')->
             limit(20)->get();
         return $variantes;
+    }
+
+    public function show($id) {
+        $variante = Variante::select('id',
+            'description','evidence')->
+            where('id',$id)->first();
+        return $variante;
     }
 }
